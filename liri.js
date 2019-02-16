abb4bec35a4a98f3c24214112a4d7ac559e209d2
Brendan K <brendanmkelly94@gmail.com>
2019-02-16 16:19:12 -0600

    Spotify call is up and running

--- liri.js
+++ liri.js
@@ -5,7 +5,8 @@ var keys = require("./keys.js");
 require("node-spotify-api");
 var axios = require("axios");
 var moment = require("moment");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
 var args = process.argv; 
 
@@ -15,12 +16,11 @@ var term = args.splice(3).join("%20");
 
 switch (action){
     case "concert-this": 
-        console.log("calling concert-this");
+        console.log("calling concert-this for " + term);
         var URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
         console.log(URL);
         axios.get(URL).then(
             function(response){
-                // console.log(response.data);
                 response.data.forEach(function(concert){
                     //from moment docs
                     var date = moment(concert.datetime, moment.ISO_8601).format("MM/DD/YYYY");
@@ -29,10 +29,19 @@ switch (action){
                     console.log("-------------------------");
                 });
         });
-        console.log(term);
         break;
     case "spotify-this-song": 
-        console.log("spotifying this");
+        console.log("searching spotify for " + term);
+        spotify.search({ type: 'track', query: term }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          console.log(
+                "Artist(s): " + data.tracks.items[0].artists[0].name +
+                "\nTitle: " + data.tracks.items[0].name +
+                "\nLink: " + data.tracks.items[0].preview_url + "\n");
+          });
         break;
     case "movie-this": 
         console.log("movie-this called");
