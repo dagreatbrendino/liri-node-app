1f48395235270698e572c947cce83467d1d0d6e7
Brendan K <brendanmkelly94@gmail.com>
2019-02-16 18:47:02 -0600

    added log.txt writer

--- liri.js
+++ liri.js
@@ -17,17 +17,34 @@ var term = args.splice(3).join("%20");
 var liriRun = function(act, query){
     switch (act){
         case "concert-this": 
-            console.log("calling concert-this for " + query);
+            console.log("calling concert-this for " + query.replace(/%20/g, " "));
             var URL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
             console.log(URL);
             axios.get(URL).then(
                 function(response){
+                    fs.appendFile("log.txt", ("concert-this\n"), function(err){
+                        if (err){
+                            throw err;
+                        }
+                    });
                     response.data.forEach(function(concert){
                         //from moment docs
                         var date = moment(concert.datetime, moment.ISO_8601).format("MM/DD/YYYY");
-                        console.log("Venue: " + concert.venue.name + " Location: " + concert.venue.city + ", " + concert.venue.region +
-                        " Date: " + date);
+                        var concertInfo = "Venue: " + concert.venue.name + " Location: " + concert.venue.city + ", " + concert.venue.region +
+                        " Date: " + date;
+                        console.log(concertInfo);
                         console.log("-------------------------");
+                        fs.appendFile("log.txt", (concertInfo + "\n------\n"), function(err){
+                            if (err){
+                                throw err;
+                            }
+                        });
+                    })
+                    fs.appendFile("log.txt", ("----------------------\n"), function(err){
+                        if (err){
+                            throw err;
+                        }
+                        console.log("Concert search added to log.txt!");
                     });
             });
             break;
@@ -35,28 +52,35 @@ var liriRun = function(act, query){
             if(query === ""){
                 query = "The%20Sign";
             }
-            console.log("searching spotify for " + query);
+            console.log("searching spotify for " + query.replace(/%20/g, " "));
             spotify.search({ type: 'track', query: query }, function(err, data) {
                 if (err) {
                   return console.log('Error occurred: ' + err);
                 }
                
-              console.log(
+                var songInfo =
                     "Artist(s): " + data.tracks.items[0].artists[0].name +
                     "\nTitle: " + data.tracks.items[0].name +
                     "\nLink: " + data.tracks.items[0].preview_url + 
-                    "\nAlbum: " + data.tracks.items[0].album.name);
+                    "\nAlbum: " + data.tracks.items[0].album.name;
+                console.log(songInfo);
+                fs.appendFile("log.txt", ("spotify-this-song\n" + songInfo + "\n----------------------\n"), function(err){
+                    if (err){
+                        throw err;
+                    }
+                    console.log("Song search added to log.txt!");
+                });
               });
             break;
         case "movie-this":
             if(query === ""){
                 query = "Mr.%20Nobody";
             } 
-            console.log("movie-this searching for " + query);
+            console.log("movie-this searching for " + query.replace(/%20/g, " "));
             var URL = `http://www.omdbapi.com/?t=${query}&y=&plot=short&apikey=trilogy`;
             axios.get(URL).then(
                 function(response) {
-                    console.log(
+                    var movieInfo = 
                         "Title: " + response.data.Title + 
                         "\nReleased: " + response.data.Released + 
                         "\nIMDB Rating: " + response.data.imdbRating + 
@@ -64,8 +88,16 @@ var liriRun = function(act, query){
                         "\nCountry: " + response.data.Country +
                         "\nLanguage: " + response.data.Language + 
                         "\nPlot: " + response.data.Plot + 
-                        "\nActors: " + response.data.Actors);
+                        "\nActors: " + response.data.Actors;
+                    console.log(movieInfo)
+                    fs.appendFile("log.txt", ("movie-this\n" +movieInfo + "\n----------------------\n"), function(err){
+                        if (err){
+                            throw err;
+                        }
+                        console.log("Movie search added to log.txt!");
+                    });
                 }
+                
             );
             break;
         case "do-what-it-says":
@@ -73,19 +105,17 @@ var liriRun = function(act, query){
                     if (err){
                         console.log(err);
                         return;
-                    }
-                    console.log(data);
+                    } 
                     var actionTerm = data.split(",")
                     var action = actionTerm[0];
                     var term = actionTerm[1];
                     liriRun(action, term);
                   
                 });
-                // console.log(term);
-            console.log("Doing what it says");
+                ;
             break;
         default: 
-            console.log("default behaviour");
+            console.log("Invalid action argument!");
     }
 
 }
